15461a071c14978774d9b6f3e6d48f1f8bfbd313
baiqyustiarini <baiqyustiarini@gmail.com>
2022-03-17 14:15:25 +0700

    lengkap

---
+++ HASH/sha128.py
@@ -0,0 +1,24 @@
+import hashlib
+from random import randint
+
+#Input integer
+# message = randint(60,100)
+# string = str(message)
+
+#Input string
+string = "MBC laboratory"
+
+#Encode
+encoded = string.encode()
+result = hashlib.sha1(encoded)
+
+#Display
+print("String\t\t\t: ", end ="")
+print(string)
+print("Hash Value\t\t: ", end ="")
+print(result)
+print("Hexadecimal equivalent\t:",result.hexdigest())
+print("Digest Size\t\t: ", end ="")
+print(result.digest_size)
+print("Block Size\t\t: ", end ="")
+print(result.block_size)
