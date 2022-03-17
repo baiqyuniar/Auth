df4532d95ecd74ee63f04c496bb0af75607b28d7
baiqyustiarini <baiqyustiarini@gmail.com>
2022-03-17 14:30:21 +0700

    enc ecc

--- ECC/EncDec.py
+++ ECC/EncDec.py
@@ -4,21 +4,20 @@ from ecc.curve import (
 from ecc.cipher import ElGamal
 from ecc.key import gen_keypair
 
-
 CURVES = [P256, secp256k1, Curve25519, M383, E222, E382]
-PLAINTEXT = b"Rini yang cantiiik"
+PLAINTEXT = b"MBC laboratory"
 
 # Generate key pair
 pri_key, pub_key = gen_keypair(E382)
+print("Private key\t: ",pri_key)
+print("Public key\t: ",pub_key)
 
 # Initiate ElGamal
 cipher_elg = ElGamal(E382)
 
 # Encrypt
 C1, C2 = cipher_elg.encrypt(PLAINTEXT, pub_key)
-print(pri_key)
-print(pub_key)
 
 # Decrypt
 plaintext = cipher_elg.decrypt(pri_key, C1, C2)
-print(plaintext)
+print("Plaintext\t: ",plaintext)
